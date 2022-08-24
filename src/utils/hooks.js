fa3cedbe1e03767992f790a77af5b616b311829b
2nthony <ant.hu@outlook.com>
2022-08-24 11:29:53 +0800

    fix: use window size

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -18,22 +18,35 @@ export function useOnClickOutside(ref, handler) {
 }
 
 export function useWindowSize() {
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  // https://github.com/jaredLunde/react-hook/blob/60a6913d1920cee9f99c94f572a88d481fcd39ed/packages/window-size/src/index.tsx#L14-L18
+  function getWindowSize() {
+    const el = document?.documentElement;
+
+    if (!el) {
+      return {
+        width: undefined,
+        height: undefined,
+      };
     }
 
+    return {
+      width: el.clientWidth,
+      height: el.clientHeight,
+    };
+  }
+
+  function handleResize() {
+    setWindowSize(getWindowSize());
+  }
+
+  useEffect(() => {
     window.addEventListener("resize", handleResize);
     handleResize();
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
   return windowSize;
 }
 
