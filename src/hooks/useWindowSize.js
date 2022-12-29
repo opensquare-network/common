3a8b2c1469b6c0fec6d9485ed0342a7c3b18af60
2nthony <ant.hu@outlook.com>
2022-12-29 18:53:08 +0800

    fix(window-size): should include the scroll bar width (#7)

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -3,20 +3,10 @@ import { useEffect, useState } from "react";
 export function useWindowSize() {
   const [windowSize, setWindowSize] = useState(getWindowSize());
 
-  // https://github.com/jaredLunde/react-hook/blob/60a6913d1920cee9f99c94f572a88d481fcd39ed/packages/window-size/src/index.tsx#L14-L18
   function getWindowSize() {
-    const el = document?.documentElement;
-
-    if (!el) {
-      return {
-        width: undefined,
-        height: undefined,
-      };
-    }
-
     return {
-      width: el.clientWidth,
-      height: el.clientHeight,
+      width: window?.innerWidth,
+      height: window?.innerHeight,
     };
   }
 
