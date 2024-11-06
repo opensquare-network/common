56d4f1c5e12da0d83a64a97e84c16f559d874939
2nthony <ant.hu@outlook.com>
2024-11-06 18:57:21 +0800

    test: update

--- test/fetchIdentity.test.js
+++ test/fetchIdentity.test.js
@@ -1,23 +1,23 @@
 import { test, expect } from "vitest";
-import {
-  fetchIdentity,
-} from "../src/services/identity";
+import { createFetchIdentity } from "../src/services/identity";
+
+const fetchIdentity = createFetchIdentity();
 
 test("fetchIdentity error", async () => {
   await expect(
-    fetchIdentity("kusama", "15wepZh1jWNqxBjsgErm8HmYiE21n79c5krQJeTsYAjHdde")
+    fetchIdentity("kusama", "15wepZh1jWNqxBjsgErm8HmYiE21n79c5krQJeTsYAjHdde"),
   ).rejects.toThrow();
 });
 
 test("fetchIdentity success", async () => {
   // Will fetch from https://id.statescan.io by default when env configuration does not exist
   await expect(
-    fetchIdentity("kusama", "GLVeryFRbg5hEKvQZcAnLvXZEXhiYaBjzSDwrXBXrfPF7wj")
+    fetchIdentity("kusama", "GLVeryFRbg5hEKvQZcAnLvXZEXhiYaBjzSDwrXBXrfPF7wj"),
   ).resolves.toEqual({
     address: "GLVeryFRbg5hEKvQZcAnLvXZEXhiYaBjzSDwrXBXrfPF7wj",
     info: {
       display: "RTTI-5220",
-      status: "VERIFIED"
-    }
+      status: "VERIFIED",
+    },
   });
 });
